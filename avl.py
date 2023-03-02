6d48621a77cd0164b244395ce27925a97ff67cdf
m-uh <126530073+m-uh@users.noreply.github.com>
2023-03-02 02:12:27 -0600

    Create avl.py

---
+++ avl.py
@@ -0,0 +1,512 @@
+
+import random
+from queue_and_stack import Queue, Stack
+from bst import BSTNode, BST
+
+
+class AVLNode(BSTNode):
+    """
+    AVL Tree Node class. Inherits from BSTNode
+    DO NOT CHANGE THIS CLASS IN ANY WAY
+    """
+    def __init__(self, value: object) -> None:
+        """
+        Initialize a new AVL node
+        DO NOT CHANGE THIS METHOD IN ANY WAY
+        """
+        # call __init__() from parent class
+        super().__init__(value)
+
+        # new variables needed for AVL
+        self.parent = None
+        self.height = 0
+
+    def __str__(self) -> str:
+        """
+        Override string method
+        DO NOT CHANGE THIS METHOD IN ANY WAY
+        """
+        return 'AVL Node: {}'.format(self.value)
+
+
+class AVL(BST):
+    """
+    AVL Tree class. Inherits from BST
+    """
+
+    def __init__(self, start_tree=None) -> None:
+        """
+        Initialize a new AVL Tree
+        DO NOT CHANGE THIS METHOD IN ANY WAY
+        """
+        # call __init__() from parent class
+        super().__init__(start_tree)
+
+    def __str__(self) -> str:
+        """
+        Override string method
+        DO NOT CHANGE THIS METHOD IN ANY WAY
+        """
+        values = []
+        super()._str_helper(self._root, values)
+        return "AVL pre-order { " + ", ".join(values) + " }"
+
+    def is_valid_avl(self) -> bool:
+        """
+        Perform pre-order traversal of the tree. Return False if there
+        are any problems with attributes of any of the nodes in the tree.
+
+        This is intended to be a troubleshooting 'helper' method to help
+        find any inconsistencies in the tree after the add() or remove()
+        operations. Review the code to understand what this method is
+        checking and how it determines whether the AVL tree is correct.
+
+        DO NOT CHANGE THIS METHOD IN ANY WAY
+        """
+        stack = Stack()
+        stack.push(self._root)
+        while not stack.is_empty():
+            node = stack.pop()
+            if node:
+                # check for correct height (relative to children)
+                left = node.left.height if node.left else -1
+                right = node.right.height if node.right else -1
+                if node.height != 1 + max(left, right):
+                    return False
+
+                if node.parent:
+                    # parent and child pointers are in sync
+                    if node.value < node.parent.value:
+                        check_node = node.parent.left
+                    else:
+                        check_node = node.parent.right
+                    if check_node != node:
+                        return False
+                else:
+                    # NULL parent is only allowed on the root of the tree
+                    if node != self._root:
+                        return False
+                stack.push(node.right)
+                stack.push(node.left)
+        return True
+
+    # ------------------------------------------------------------------ #
+
+    def add(self, value: object) -> None:
+        """
+        adds a new value to the tree while maintaining its AVL property.
+        Duplicate values are not allowed.
+        If the value is already in the tree, the method should not change the tree.
+        """
+        # if root is none, set = AVLNode value and return
+        if self._root is None:
+            self._root = AVLNode(value)
+            return
+
+        # set value = root value
+        if value == self._root.value:
+            return
+
+        current = self._root
+        new_node = AVLNode(value)
+        parent_node = None
+
+        while current is not None:
+            # set parent = to current
+            parent_node = current
+
+            # if value = current value, return
+            if value == current.value:
+                return
+
+            # if value > current value, set = to right node
+            if value > current.value:
+                current = current.right
+            # else, set = to left node
+            else:
+                current = current.left
+
+        # if value > parent value, set = to new_node
+        if value > parent_node.value:
+            parent_node.right = new_node
+            parent_node.right.parent = parent_node
+
+        # if value < parent value, set = to new_node
+        if value < parent_node.value:
+            parent_node.left = new_node
+            parent_node.left.parent = parent_node
+
+        #  re-balance and set = to parent
+        while parent_node is not None:
+            self._rebalance(parent_node)
+            parent_node = parent_node.parent
+
+    def remove(self, value: object) -> bool:
+        """
+        removes the value from the AVL tree.
+        The method returns True if the value is removed.
+        Otherwise, it returns False
+        """
+        if not self.contains(value):
+            return False
+
+            # return and call helper
+        self._root = self.remove_helper(self._root, value)
+        return True
+        # remove pass
+
+    def remove_helper(self, first_node, cue):
+        """
+        remove helper function
+        """
+        # look for root, return none
+        if not first_node:
+            return None
+
+        # if, 'cue' is < value, then look for cue on left tree
+        if first_node.value > cue:
+            first_node.left = self.remove_helper(first_node.left, cue)
+
+        # else if 'cue' is > value, then look for cue on right tree
+        elif first_node.value < cue:
+            first_node.right = self.remove_helper(first_node.right, cue)
+
+        # if neither, then cue = value
+        else:
+            # if first_node (root) is on left and right
+            if first_node.left and first_node.right:
+
+                # and if right tree has far left node
+                if first_node.right.left:
+                    # set =
+                    farleft = first_node.right.left
+                    # and set the farthest left root
+                    leftroot = first_node.right
+
+                    # find parent of far left node
+                    while farleft.left:
+                        farleft = farleft.left
+                        leftroot = leftroot.left
+
+                    # leftmost parent = left node
+                    leftroot.left = farleft.right
+                    # left far left and right far left = left root and right root, and replace left
+                    farleft.left, farleft.right = first_node.left, first_node.right
+                    first_node = farleft
+
+                # else no left tree, replace right instead
+                else:
+                    first_node.right.left = first_node.left
+                    first_node = first_node.right
+
+            # if only one subnode on left, still replace
+            elif first_node.left:
+                first_node = first_node.left
+                # if only one subnode on right, still replace
+            elif first_node.right:
+                first_node = first_node.right
+                # else, = None
+            else:
+                first_node = None
+
+        return first_node
+
+    def _balance_factor(self, node: AVLNode) -> int:
+        """
+        returns balance after node
+        """
+        return self._get_height(node.right) - self._get_height(node.left)
+
+    def _get_height(self, node: AVLNode) -> int:
+        """
+        gets and returns node height
+        """
+        if node is None:
+            return -1
+
+        # gets heights
+        left_height = self._get_height(node.left)
+        right_height = self._get_height(node.right)
+
+        # adds heights to get max
+        return max(left_height, right_height) + 1
+
+    def _rotate_left(self, node: AVLNode) -> AVLNode:
+        """
+        returns node after rotating left
+        """
+        # sets variables and nodes
+        subnode = node.right
+        node.parent = subnode
+        node.right = subnode.left
+        subnode.left = node
+
+        # if is not none, set =
+        if node.right is not None:
+            node.right.parent = node
+
+        # updates heights and returns
+        self._update_height(node)
+        self._update_height(subnode)
+        return subnode
+
+    def _rotate_right(self, node: AVLNode) -> AVLNode:
+        """
+        returns node after rotating right
+        """
+        # sets variables and nodes
+        subnode = node.left
+        node.left = subnode.right
+        node.parent = subnode
+        subnode.right = node
+
+        # if is not none, set =
+        if node.left is not None:
+            node.left.parent = node
+
+        # update heights and return
+        self._update_height(node)
+        self._update_height(subnode)
+        return subnode
+
+    def _update_height(self, node: AVLNode) -> None:
+        """
+        gets and updates node height
+        """
+        node.height = max(self._get_height(node.left), self._get_height(node.right)) + 1
+
+    def _rebalance(self, node: AVLNode) -> None:
+        """
+        Re-balances node
+        """
+        # if, balance of node > 1
+        if self._balance_factor(node) > 1:
+            # of balance of right < 0
+            if self._balance_factor(node.right) < 0:
+                # right node rotate right and set parent =
+                node.right = self._rotate_right(node.right)
+                node.right.parent = node
+
+            # sets parent and root
+            parent_node = node.parent
+            first_node = self._rotate_left(node)
+            # sets root to parent
+            first_node.parent = parent_node
+
+            # if parent is none, set self._root = to root
+            if parent_node is None:
+                self._root = first_node
+                return
+            # if parent value > root value, set =
+            if parent_node.value > first_node.value:
+                parent_node.left = first_node
+            # else, set right
+            else:
+                parent_node.right = first_node
+
+        # else, if balance of node < -1
+        elif self._balance_factor(node) < -1:
+            # if balance left node > 0, set = to rotated
+            if self._balance_factor(node.left) > 0:
+                node.left = self._rotate_left(node.left)
+                node.left.parent = node
+
+            # saves and sets parent
+            parent_node = node.parent
+            first_node = self._rotate_right(node)
+            # set parent for node
+            first_node.parent = parent_node
+
+            # if parent node is none, set self._root = root
+            if parent_node is None:
+                self._root = first_node
+                return
+            # if parent value > root, set left
+            if parent_node.value > first_node.value:
+                parent_node.left = first_node
+            # else, set right
+            else:
+                parent_node.right = first_node
+
+        # else, update height of node
+        else:
+            self._update_height(node)
+
+# ------------------- BASIC TESTING -----------------------------------------
+
+
+if __name__ == '__main__':
+
+    print("\nPDF - method add() example 1")
+    print("----------------------------")
+    test_cases = (
+        (1, 2, 3),  # RR
+        (3, 2, 1),  # LL
+        (1, 3, 2),  # RL
+        (3, 1, 2),  # LR
+    )
+    for case in test_cases:
+        tree = AVL(case)
+        print(tree)
+
+    print("\nPDF - method add() example 2")
+    print("----------------------------")
+    test_cases = (
+        (10, 20, 30, 40, 50),   # RR, RR
+        (10, 20, 30, 50, 40),   # RR, RL
+        (30, 20, 10, 5, 1),     # LL, LL
+        (30, 20, 10, 1, 5),     # LL, LR
+        (5, 4, 6, 3, 7, 2, 8),  # LL, RR
+        (range(0, 30, 3)),
+        (range(0, 31, 3)),
+        (range(0, 34, 3)),
+        (range(10, -10, -2)),
+        ('A', 'B', 'C', 'D', 'E'),
+        (1, 1, 1, 1),
+    )
+    for case in test_cases:
+        tree = AVL(case)
+        print('INPUT  :', case)
+        print('RESULT :', tree)
+
+    print("\nPDF - method add() example 3")
+    print("----------------------------")
+    for _ in range(100):
+        case = list(set(random.randrange(1, 20000) for _ in range(900)))
+        tree = AVL()
+        for value in case:
+            tree.add(value)
+        if not tree.is_valid_avl():
+            raise Exception("PROBLEM WITH ADD OPERATION")
+    print('add() stress test finished')
+
+    print("\nPDF - method remove() example 1")
+    print("-------------------------------")
+    test_cases = (
+        ((1, 2, 3), 1),  # no AVL rotation
+        ((1, 2, 3), 2),  # no AVL rotation
+        ((1, 2, 3), 3),  # no AVL rotation
+        ((50, 40, 60, 30, 70, 20, 80, 45), 0),
+        ((50, 40, 60, 30, 70, 20, 80, 45), 45),  # no AVL rotation
+        ((50, 40, 60, 30, 70, 20, 80, 45), 40),  # no AVL rotation
+        ((50, 40, 60, 30, 70, 20, 80, 45), 30),  # no AVL rotation
+    )
+    for case, del_value in test_cases:
+        tree = AVL(case)
+        print('INPUT  :', tree, "DEL:", del_value)
+        tree.remove(del_value)
+        print('RESULT :', tree)
+
+    print("\nPDF - method remove() example 2")
+    print("-------------------------------")
+    test_cases = (
+        ((50, 40, 60, 30, 70, 20, 80, 45), 20),  # RR
+        ((50, 40, 60, 30, 70, 20, 80, 15), 40),  # LL
+        ((50, 40, 60, 30, 70, 20, 80, 35), 20),  # RL
+        ((50, 40, 60, 30, 70, 20, 80, 25), 40),  # LR
+    )
+    for case, del_value in test_cases:
+        tree = AVL(case)
+        print('INPUT  :', tree, "DEL:", del_value)
+        tree.remove(del_value)
+        print('RESULT :', tree)
+
+    print("\nPDF - method remove() example 3")
+    print("-------------------------------")
+    case = range(-9, 16, 2)
+    tree = AVL(case)
+    for del_value in case:
+        print('INPUT  :', tree, del_value)
+        tree.remove(del_value)
+        print('RESULT :', tree)
+
+    print("\nPDF - method remove() example 4")
+    print("-------------------------------")
+    case = range(0, 34, 3)
+    tree = AVL(case)
+    for _ in case[:-2]:
+        root_value = tree.get_root().value
+        print('INPUT  :', tree, root_value)
+        tree.remove(root_value)
+        print('RESULT :', tree)
+
+    print("\nPDF - method remove() example 5")
+    print("-------------------------------")
+    for _ in range(100):
+        case = list(set(random.randrange(1, 20000) for _ in range(900)))
+        tree = AVL(case)
+        for value in case[::2]:
+            tree.remove(value)
+        if not tree.is_valid_avl():
+            raise Exception("PROBLEM WITH REMOVE OPERATION")
+    print('remove() stress test finished')
+
+    print("\nPDF - method contains() example 1")
+    print("---------------------------------")
+    tree = AVL([10, 5, 15])
+    print(tree.contains(15))
+    print(tree.contains(-10))
+    print(tree.contains(15))
+
+    print("\nPDF - method contains() example 2")
+    print("---------------------------------")
+    tree = AVL()
+    print(tree.contains(0))
+
+    print("\nPDF - method inorder_traversal() example 1")
+    print("---------------------------------")
+    tree = AVL([10, 20, 5, 15, 17, 7, 12])
+    print(tree.inorder_traversal())
+
+    print("\nPDF - method inorder_traversal() example 2")
+    print("---------------------------------")
+    tree = AVL([8, 10, -4, 5, -1])
+    print(tree.inorder_traversal())
+
+    print("\nPDF - method find_min() example 1")
+    print("---------------------------------")
+    tree = AVL([10, 20, 5, 15, 17, 7, 12])
+    print(tree)
+    print("Minimum value is:", tree.find_min())
+
+    print("\nPDF - method find_min() example 2")
+    print("---------------------------------")
+    tree = AVL([8, 10, -4, 5, -1])
+    print(tree)
+    print("Minimum value is:", tree.find_min())
+
+    print("\nPDF - method find_max() example 1")
+    print("---------------------------------")
+    tree = AVL([10, 20, 5, 15, 17, 7, 12])
+    print(tree)
+    print("Maximum value is:", tree.find_max())
+
+    print("\nPDF - method find_max() example 2")
+    print("---------------------------------")
+    tree = AVL([8, 10, -4, 5, -1])
+    print(tree)
+    print("Maximum value is:", tree.find_max())
+
+    print("\nPDF - method is_empty() example 1")
+    print("---------------------------------")
+    tree = AVL([10, 20, 5, 15, 17, 7, 12])
+    print("Tree is empty:", tree.is_empty())
+
+    print("\nPDF - method is_empty() example 2")
+    print("---------------------------------")
+    tree = AVL()
+    print("Tree is empty:", tree.is_empty())
+
+    print("\nPDF - method make_empty() example 1")
+    print("---------------------------------")
+    tree = AVL([10, 20, 5, 15, 17, 7, 12])
+    print("Tree before make_empty():", tree)
+    tree.make_empty()
+    print("Tree after make_empty(): ", tree)
+
+    print("\nPDF - method make_empty() example 2")
+    print("---------------------------------")
+    tree = AVL()
+    print("Tree before make_empty():", tree)
+    tree.make_empty()
+    print("Tree after make_empty(): ", tree)
